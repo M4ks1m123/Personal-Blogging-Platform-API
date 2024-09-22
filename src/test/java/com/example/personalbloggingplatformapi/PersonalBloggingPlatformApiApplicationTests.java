c1ac8faea9da9531e3c3eddefbef72da2cfd9a55
M4ks1m123 <maximalian67@gmail.com>
2024-09-22 23:36:21 +0300

    init commit

---
+++ src/test/java/com/example/personalbloggingplatformapi/PersonalBloggingPlatformApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.personalbloggingplatformapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalBloggingPlatformApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
